adf55600e048c7e5df591e90e618471427d47582
Andres Soria <andres.zov@gmail.com>
2022-08-29 14:50:09 -0500

    Create funtion to search and append tv show images

---
+++ app.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("#search-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const config = {
+    params: { q: searchTerm },
+  };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  makeImages(res.data);
+  form.elements.query.value = "";
+});
+
+function makeImages(shows) {
+  for (const result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("img");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+}
